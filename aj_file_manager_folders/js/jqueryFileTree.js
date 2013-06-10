46b93b212dfa592456604a672e4018762eae968a
Diana <diana@ajency.in>
2013-06-10 17:38:17 +0530

    revertrd loader img

--- aj_file_manager_folders/js/jqueryFileTree.js
+++ aj_file_manager_folders/js/jqueryFileTree.js
@@ -51,9 +51,9 @@ if(jQuery) (function($){
 				
 				function showTree(c, t) { 
 					$(c).addClass('wait');
-					//$(".jqueryFileTree.start").remove(); commented and added after one line below to display loading img
+					$(".jqueryFileTree.start").remove(); //commented and added after one line below to display loading img
 					$.post(o.script, { dir: t ,action:'intellidocs_fetch_tree',catid:$(c).attr('data-catid')}, function(data) {
-						$(".jqueryFileTree.start").remove();
+						//$(".jqueryFileTree.start").remove();
 						$(c).find('.start').html('');
 						$(c).removeClass('wait').append(data);
 						if( o.root == t ) $(c).find('UL:hidden').show(); else $(c).find('UL:hidden').slideDown({ duration: o.expandSpeed, easing: o.expandEasing });
