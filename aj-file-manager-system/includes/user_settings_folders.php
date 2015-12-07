ceacf3ace5112975872142aae923f950207fff09
Robiul Hoque <robiul@ajency.in>
2015-12-07 15:21:24 +0530

    folder visibility issue fixed.

--- aj-file-manager-system/includes/user_settings_folders.php
+++ aj-file-manager-system/includes/user_settings_folders.php
@@ -5,13 +5,13 @@
 add_action( 'show_user_profile', 'dmt_extend_profile_fields' );
 add_action( 'edit_user_profile', 'dmt_extend_profile_fields' );
 
-function dmt_extend_profile_fields( $user ) { 
+function dmt_extend_profile_fields( $user ) {
 
 	if(get_current_user_id()==$user->ID)
 	{
 	return false;
 	}
-	
+
  $selected_folders = get_user_meta($user->ID, 'dmt_folder_access');
  $nested_args= array(
 			'descendants_and_self' => 0,
@@ -20,8 +20,8 @@ function dmt_extend_profile_fields( $user ) {
 			'walker' => null,
 			'taxonomy' => 'document_folders',
 			'checked_ontop' => false,
-	); 
- 
+	);
+
  $parent_args = array(
  	'type'                     => 'post',
 	'child_of'                 => 0,
@@ -33,36 +33,42 @@ function dmt_extend_profile_fields( $user ) {
 	'taxonomy'                 => 'document_folders',
 	'pad_counts'               => false
  	);
-	
-	
+
+
 	if(!current_user_can('administrate'))
 	{
 		$push = get_all_user_folders($show = false);
-		
+
 		if(count($push)!=0)
 		{
-				
+
 		$args1 = array(
-		'hide_empty'    => false, 
+		'hide_empty'    => false,
 		'include'       => $push
-		); 
+		);
 			$categories =  get_terms('document_folders',  $args1);
 		}
-	
+
 	}
 	else
 	{
 		$args1 = array(
 		'orderby'    => 'name',
 		'hide_empty' => 0,
-		'parent' =>0, 
+		'parent' =>0,
 		'hierarchical'  => true
-		); 
+		);
 		$categories =  get_terms('document_folders',  $args1);
 	}
-	
+
+//
+// print_r($selected_folders);
+// foreach($categories as $category){
+// 	echo $category->term_id.'<br />';
+// }
+
 ?>
-<?php 
+<?php
 /**
  * Show this function in between
  * <ul class="dmtDocumentFolderStructure"></ul>
@@ -71,7 +77,7 @@ function dmt_extend_profile_fields( $user ) {
 //wp_terms_checklist(0, $nested_args); ?>
 <?php //$categories = get_categories( $parent_args ); ?>
 	<h3>Folder Visibility</h3>
-	
+
 	<table class="form-table">
 
 	<tr>
@@ -84,7 +90,8 @@ function dmt_extend_profile_fields( $user ) {
 if(!is_null($categories)){
 	 foreach($categories as $category):?>
 				<?php if($selected_folders[0]['document_folders']): ?>
-					<?php $checked = (in_array($category->term_id,$selected_folders[0]['document_folders']))?'checked="checked"':''; ?>
+					<?php $checked = (in_array($category->term_id,$selected_folders[0]['document_folders']))?'checked="checked"':'';
+				 ?>
 				<?php endif;?>
 				<li id="document_folders-<?php echo $category->term_id;?>">
 					<label class="selectit">
@@ -109,7 +116,7 @@ if(!is_null($categories)){
 /**
  * Function to save the changes for the folders visible to the current user
  */
- 
+
 add_action( 'personal_options_update', 'dmt_extend_profile_fields_save' );
 add_action( 'edit_user_profile_update', 'dmt_extend_profile_fields_save' );
 
@@ -117,13 +124,13 @@ function dmt_extend_profile_fields_save($user_id) {
 
 	if ( (!current_user_can( 'edit_user')) || (get_current_user_id()==$user->ID) )
 		return false;
-	
+
 	update_usermeta( $user_id, 'dmt_folder_access', $_POST['tax_input'] );
-	
+
 	global $wpdb;
 	$table_name = $wpdb->prefix . "dmt_user_cat_access_data";
-	
-	$wpdb->query( $wpdb->prepare( 
+
+	$wpdb->query( $wpdb->prepare(
 						"
 							DELETE FROM $table_name
 						 	WHERE user_id = %d
@@ -137,6 +144,3 @@ function dmt_extend_profile_fields_save($user_id) {
 		 $wpdb->insert( $table_name, array( 'user_id' => $user_id, 'category_id' => $category ) );
 	}
 }
-
-
-		        
--- aj_file_manager_folders/js/dmtTreeFolderStructureModel.js
+++ aj_file_manager_folders/js/dmtTreeFolderStructureModel.js
@@ -1,18 +1,18 @@
 
 
-function funconfirmaction(rel,folder_name,folder_id){ 
+function funconfirmaction(rel,folder_name,folder_id){
 
 	folder = jQuery(this).attr("folder-name")
 	ret = confirm("Are you sure you want to delete the folder "+folder_name+" ?")
 	if(ret==true)
-	{  
+	{
 
 
-		var folder_data = 
+		var folder_data =
 		{
 				action 			: 'dmt_ajax_folder_delete',
 				recursive 			: 'no',
-				folder_id 		: folder_id 
+				folder_id 		: folder_id
 		};
 
 		jQuery.post(ajaxurl, folder_data, function(response) {
@@ -20,7 +20,7 @@ function funconfirmaction(rel,folder_name,folder_id){
 			if(response.success == true)
 			{
 				alert(folder_name+" Folder Deleted Successfully!")
-				location.reload();	
+				location.reload();
 			}
 			else
 			{
@@ -28,16 +28,16 @@ function funconfirmaction(rel,folder_name,folder_id){
 				alert('Something seems to have gone wrong with the delete. Try again later.');
 			}
 		});
-	} 
-}	
+	}
+}
 
 
 jQuery('.add-sub-folder').live('click', function(e) {
-	jQuery("#add-folder-div").remove() 
+	jQuery("#add-folder-div").remove()
 	html = "<div style='display:inline' id='add-folder-div'><hr><table><tr><td><b>Folder Name</b>:</td><td><input type='text' id='folder-name'> <input type='hidden' id='parent-folder' value='"+jQuery("#"+e.target.id).attr("parent-folder")+"'></td></tr><tr><td><b>Description</b>:</td>  <td><textarea id='folder-desc' rows='5' cols='50'></textarea></td></tr></table><input type='button' id='create-sub-folder' value='Create' class='button button-primary'> <input type='button' id='cancel-add-folder' value='Cancel' class='button button-primary'><hr></div>"
 	if (jQuery("#"+e.target.id).attr("parent-folder")==0)
 	{
-		jQuery("#"+e.target.id).after(html) 
+		jQuery("#"+e.target.id).after(html)
 	}
 	else
 	{
@@ -46,27 +46,27 @@ jQuery('.add-sub-folder').live('click', function(e) {
 
 });
 jQuery('#cancel-add-folder').live('click', function(e) {
-	jQuery("#add-folder-div").remove() 
+	jQuery("#add-folder-div").remove()
 });
 
 jQuery('#create-sub-folder').live('click', function(e) {
-	
+
 	if(jQuery("#folder-name").val()=="")
 		{
 
 		alert("Please Enter Folder Name")
-		
+
 		return false;
 		}
 	folder_name = jQuery("#folder-name").val()
 	window.selected_folder = jQuery("#folder-name").val();
-	localStorage.setItem('selected_folder',jQuery("#folder-name").val())		
+	localStorage.setItem('selected_folder',jQuery("#folder-name").val())
 	parent_folder = jQuery("#parent-folder").val()
 	folder_desc =   jQuery("#folder-desc").val()
 	jQuery("#add-folder-div").after("<div style='display:inline' id='msg-folder-div"+parent_folder+"'>Creating Sub Folder.....</div>")
-	jQuery("#add-folder-div").remove() 
+	jQuery("#add-folder-div").remove()
 
-	var folder_data = 
+	var folder_data =
 	{
 		action 			: 'dmt_ajax_create_sub_folder',
 		folder_name 	: folder_name,
@@ -81,9 +81,9 @@ jQuery('#create-sub-folder').live('click', function(e) {
 			jQuery("#msg-folder-div"+parent_folder+"").remove()
 			alert(folder_name+" Folder Created Successfully!")
 			if(parseInt(parent_folder) == 0)
-			location.href = SITEURL+'/wp-admin/edit.php?post_type=document_files&page=add-division'
+			location.href = SITEURL+'/wp-admin/edit.php?post_type=document_files&page=add-division';
 			else
-			location.reload();	
+			location.reload();
 		}
 		else
 		{
@@ -93,5 +93,3 @@ jQuery('#create-sub-folder').live('click', function(e) {
 	});
 
 });
-
-
